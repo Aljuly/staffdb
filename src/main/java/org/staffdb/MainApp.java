3278240bfc66c8d59ab6bd4ea8c997e37c73379d
Aljuly <alg252@yandex.ru>
2018-03-20 16:34:19 +0200

    Apply css styles to dialogs

--- src/main/java/org/staffdb/MainApp.java
+++ src/main/java/org/staffdb/MainApp.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -40,6 +41,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("staffdb");
+
+        //Set an Icon
+        this.primaryStage.getIcons().add(new Image("/images/Address_Book.png"));
 
         initRootLayout();
 
